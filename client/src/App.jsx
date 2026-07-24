1c95e9f59345feff1a0b886897c4fd69b3faceb1
kavyareddy1313 <kavyasrireddy28@gmail.com>
2026-07-25 01:43:06 +0530

    refactor: clean up App.jsx comments part 6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -256,7 +256,6 @@ export default function App() {
           </div>
         )}
 
-        {/* Skeleton State */}
         {appState === "analyzing" && (
           <div className="w-full mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 animate-pulse">
             {[1, 2, 3].map((i) => (
